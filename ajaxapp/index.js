ecae68933c7536a00b7b7d939266f5f9c19fe8f2
64-HiroByte <higher.ground.rc@gmail.com>
2025-08-09 18:17:04 +0900

    HTTP通信について学習した

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,24 +1,34 @@
 console.log('index.js: loaded');
 
-// CSSセレクタを使ってDOMツリー中のh2要素を取得する
-const heading = document.querySelector('h2');
+// // CSSセレクタを使ってDOMツリー中のh2要素を取得する
+// const heading = document.querySelector('h2');
 
-// h2要素に含まれるテキストコンテンツを取得する
-const headingText = heading.textContent;
+// // h2要素に含まれるテキストコンテンツを取得する
+// const headingText = heading.textContent;
 
-// button要素を作成する
-const button = document.createElement('button');
-button.textContent = 'Push Me';
+// // button要素を作成する
+// const button = document.createElement('button');
+// button.textContent = 'Push Me';
 
-// body要素の子要素としてbuttonを挿入する
-document.body.appendChild(button);
+// // body要素の子要素としてbuttonを挿入する
+// document.body.appendChild(button);
 
-const userID = 'js-primer-example';
-fetch(`https://api.github.com/users/${encodeURIComponent(userID)}`)
-  .then(response => {
-    console.log(response.status);
-    return response.json().then(userInfo => {
-      // JSONパースされたオブジェクトが渡される
-      console.log(userInfo);
+// const userID = 'js-primer-example';
+
+function fetchUserInfo(userID) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userID)}`)
+    .then(response => {
+      console.log(response.status);
+      // エラーレスポンスが返されたことを検知する
+      if (!response.ok) {
+        console.error('エラーレスポンス', response);
+      } else{
+        return response.json().then(userInfo => {
+          // JSONパースされたオブジェクトが渡される
+          console.log(userInfo);
+        });
+      }
+    }).catch(error => {
+      console.error(error);
     });
-  });
+}
