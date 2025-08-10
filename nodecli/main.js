edbde39d97525306e8e6b3951e4e12e7e2883bf7
64-HiroByte <higher.ground.rc@gmail.com>
2025-08-10 16:09:11 +0900

    `node:util` モジュールをインポートし、コマンドライン引数を parseArgs 関数でパースする設定を行った

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+// `node:util` モジュールを utilオブジェクトとしてインポートする
+
+import * as util from 'node:util';
+
+// コマンドライン引数を parseArgs 関数でパースする
+const {
+  values,
+  positionals
+} = util.parseArgs({
+  // オプションやフラグ以外の引数を渡すことを許可する
+  allowPositionals: true
+});
+// console.log(values);  // オプションやフラグを含むオブジェクト
+// console.log(positionals);  // フラグ以外の引数の配列
+
+const filePath = positionals[0];
+console.log(filePath);
+
