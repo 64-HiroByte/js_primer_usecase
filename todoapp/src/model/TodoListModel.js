d5a903ea1d7b75543b12d936e9632810551a390a
64-HiroByte <higher.ground.rc@gmail.com>
2025-08-13 01:11:36 +0900

    イベントとモデルについて学習、モデルを定義した。

---
+++ todoapp/src/model/TodoListModel.js
@@ -0,0 +1,52 @@
+import { EventEmitter} from '../EventEmitter.js';
+
+export class TodoListModel extends EventEmitter {
+  #items;
+  /**
+   * @param {TodoItemModel[]}  // [items] 初期アイテム一覧（デフォルトは空の配列）
+   */
+  constructor(items = []) {
+    super();
+    this.#items = items;
+  }
+
+  /**
+   * Todo Itemの合計個数を返す
+   * ＠returns {number}
+   */
+  getTotalCount() {
+    return this.#items.length;
+  }
+
+  /**
+   * 表示できるTodo Itemの配列を返す
+   * ＠returns {number}
+   */
+  getTodoItems() {
+    return this.#items;
+  }
+
+  /**
+   * TodoListの状態が更新されたときに呼び出されるリスナー関数を登録する
+   * @param {Function} listener;
+   */
+  onChange(listener) {
+    this.addEventListener('change', listener);
+  }
+
+  /**
+   * 状態が更新されたときに呼ぶ。登録済みのリスナー関数を呼び出す
+   */
+  emitChange() {
+    this.this.emit('change');
+  }
+
+  /**
+   * TodoItemを追加する
+   * @param {TodoItemModel} TodoItem;
+   */
+  addTodo(todoItem) {
+    this.#items.push(todoItem);
+    this.emitChange();
+  }
+}
